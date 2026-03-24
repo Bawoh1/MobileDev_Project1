dfed5a2474ed9a41204255d512cb381291aa6387
Bawoh1 <bryantawoh1@gmail.com>
2026-03-24 13:52:11 -0400

    Add streak tracking using SharedPreferences

--- lib/Screens/home_screen.dart
+++ lib/Screens/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import '../Data/database_helper.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -10,11 +11,13 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   List<Map<String, dynamic>> workouts = [];
+  int streak = 0;
 
   @override
   void initState() {
     super.initState();
     loadWorkouts();
+    loadStreak();
   }
 
   Future<void> loadWorkouts() async {
@@ -24,10 +27,25 @@ class _HomeScreenState extends State<HomeScreen> {
     });
   }
 
+  Future<void> loadStreak() async {
+  final prefs = await SharedPreferences.getInstance();
+  setState(() {
+    streak = prefs.getInt('streak') ?? 0;
+  });
+}
+
   Future<void> addWorkout() async {
-    await DatabaseHelper.instance.createWorkout("New Workout");
-    loadWorkouts();
-  }
+  await DatabaseHelper.instance.createWorkout("New Workout");
+
+  final prefs = await SharedPreferences.getInstance();
+  int current = prefs.getInt('streak') ?? 0;
+  current++;
+
+  await prefs.setInt('streak', current);
+
+  loadStreak();
+  loadWorkouts();
+}
 
   Future<void> deleteWorkout(int id) async {
     await DatabaseHelper.instance.deleteWorkout(id);
@@ -44,6 +62,10 @@ class _HomeScreenState extends State<HomeScreen> {
         padding: const EdgeInsets.all(16),
         child: Column(
           children: [
+              Text(
+                "Current Streak: $streak days",
+                style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
             ElevatedButton(
               onPressed: addWorkout,
               child: const Text('Start Workout'),
